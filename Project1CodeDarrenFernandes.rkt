e632ca3a737fe4140a87b8c7c5fa55fbd303c580
SHRUIKAN\darre <darren.k.fernandes@gmail.com>
2025-10-20 23:55:07 -0500

    Added more Racket calculator code

--- Project1CodeDarrenFernandes.rkt
+++ Project1CodeDarrenFernandes.rkt
@@ -33,8 +33,35 @@
                  (displayln "Error: Invalid expression")
                  (main-loop interactive? history)))))])))
 
+(define (eval-expression history char-list)
+  (let ([trimmed (remove-leading-whitespace char-list)])
+    (when (null? trimmed)
+      (error "Invalid Expression"))
+    (let ([token (car trimmed)] [rest (cdr trimmed)])
+      (cond
+        ;multiple and add have the same structure so they can be passed into the same function
+        [(char=? token #\+) (parse-binary + history rest)] 
+        [(char=? token #\*) (parse-binary * history rest)]
+        ;divide has to check if there is a zero in the denominator, so we pass it into a seperate function
+        [(char=? token #\/) (parse-division history rest)]
+        [(char=? token #\-)
+         (if (and (not (null? rest)) (char-numeric? (car rest)));if rest is not null, and the next value is a number (not a whitespace), it is a negative number
+             (parse-number trimmed)
+             (parse-unary - history rest))];
+        [(char=? token #\$) (parse-history history rest)]
+        [(char-numeric? token) (parse-number trimmed)];for when there is an expression
+        [else (error "Invalid-expression")]))))
+
+;check if all the characters are whitespaces/nothing left to evaluat
+(define (all-whitespace? char-list)
+  (if (null? char-list) #t;base case
+      (and (char-whitespace? (car char-list)) (all-whitespace? (cdr char-list)))));check if the car and rest of the list is a whitespace
+
 
 (define (remove-leading-whitespace char-list)
   (if (or (null? char-list) (not (char-whitespace? (car char-list))))
       char-list ;if the list is empty or doesn't start with whitespace, return it.
-      (remove-leading-whitespace (cdr char-list)))) 
+      (remove-leading-whitespace (cdr char-list))))
+
+
+
